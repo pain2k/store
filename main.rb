b99a6140ee3fe57356752a05bbf2abda2beac213
Mr.Pain <ramil.talipov@gmail.com>
2017-05-08 15:09:24 +0300

    version 28-5

--- main.rb
+++ main.rb
@@ -14,12 +14,8 @@ require_relative 'lib/product_collection'
 
 collection = ProductCollection.from_dir(File.dirname(__FILE__) + '/data')
 
-# Сортируем продукты по возрастанию цены с помощью метода sort! экземпляра
-# класса ProductCollection
 collection.sort!(by: :price, order: :asc)
 
-# Получаем массив продуктов методом to_a и выводим каждый на экран, передавая
-# его методу puts в качестве аргумента.
 collection.to_a.each do |product|
   puts product
 end
