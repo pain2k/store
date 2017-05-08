e6e417a4ccf6c994fdeba77b8a6362d24826280b
Mr.Pain <ramil.talipov@gmail.com>
2017-05-08 15:58:32 +0300

    version 28-5

--- main.rb
+++ main.rb
@@ -17,6 +17,28 @@ collection = ProductCollection.from_dir(File.dirname(__FILE__) + '/data')
 
 collection.sort!(by: :price, order: :asc)
 
-collection.to_a.each do |product|
-  puts product
+puts "Что хотите купить:"
+
+sum = 0
+purchase = []
+user_choice = nil
+until user_choice == 0 do
+  collection.to_a.each_with_index do |product, index|
+    puts "#{index + 1}. #{product}"
+  end
+  puts "0. Выход"
+  print ">"
+  user_choice = STDIN.gets.to_i
+  if user_choice == 0
+    break
+  else
+    sum += collection.to_a[user_choice - 1].price
+    purchase << collection.to_a[user_choice - 1].to_s
+    puts "Вы выбрали товаров на сумму: #{sum} руб."
+  end
 end
+puts "Вы купили:"
+puts purchase
+puts "С Вас - #{sum} руб. Спасибо за покупки!"
+
+
