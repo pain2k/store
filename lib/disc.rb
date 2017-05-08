7fd935a247c451c1d900e6f9662d799a2ce46f18
Mr.Pain <ramil.talipov@gmail.com>
2017-05-08 15:19:56 +0300

    version 28-5

---
+++ lib/disc.rb
@@ -0,0 +1,27 @@
+class Disc < Product
+  attr_accessor :title, :year, :performer
+
+  def self.from_file(file_path)
+    lines = File.readlines(file_path, encoding: 'UTF-8')
+
+    self.new(
+      title: lines[0].chomp,
+      performer: lines[1].chomp,
+      genre: lines[2].chomp,
+      year: lines[3].to_i,
+      price: lines[4].to_i,
+      amount: lines[5].to_i
+    )
+  end
+
+  def initialize(params)
+    super
+    @title = params[:title]
+    @year = params[:year]
+    @performer = params[:performer]
+  end
+
+  def to_s
+    "Диск #{@title}, #{@year}, #{@genre}, исп. #{@performer}, #{super}"
+  end
+end
